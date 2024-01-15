21ff67709ebb5864dbdf9aef6b1a5c3eb8317ad8
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-01-15 09:01:08 +0100

    Added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
+import "./index.css";
+import Menu from "./components/menu";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
+        <div className="container">
             <Header />
-        </>
+            <Menu />
+            <Footer />
+        </div>
     );
 }
 
