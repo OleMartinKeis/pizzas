e4b62942b329654c79acadd2ce2fe43d2a794fba
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-01-22 13:30:55 +0100

    Updated menu

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import Pizza from "../pizza";
+import pizzaData from "/data";
 
 function Menu() {
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
     return (
         <main className="menu">
             <h2>Our menu</h2>
-            <Pizza
-                name="Pizza Spinaci"
-                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-                photoName="src\assets\spinaci.jpg"
-                price="10"
-            />
-            <Pizza
-                name="Pizza Funghi"
-                ingredients="Tomato, mushrooms"
-                price="12"
-                photoName="src\assets\funghi.jpg"
-            />
+
+            {numPizzas > 0 ? (
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose
+                        from. All from our stone oven, all organic, all
+                        delicious.
+                    </p>
+                    <ul className="pizzas">
+                        {pizzaData.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <p>We're still working on our menu. Please come back later</p>
+            )}
         </main>
     );
 }
